f17c6752ccd378b974d5c6d9992eebcbd423aab5
bodia1132 <bodia.baran.o@gmail.com>
2022-07-18 20:46:36 +0300

    fractal

---
+++ cmake-build-debug/CMakeFiles/Fractal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fractal.dir/main.cpp.o"
+  "Fractal"
+  "Fractal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fractal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
